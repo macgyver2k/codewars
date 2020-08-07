53e564aaac4f3f075f9cff8c7a60affd60e96ce3
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-08-08 00:12:16 +0200

    implemented YourOrder

---
+++ Challenges/YourOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class YourOrder
+{
+    public static String Order( String words )
+    {
+        return words.Length == 0
+            ? String.Empty
+            : words
+            .Split( ' ' )
+            .OrderBy( word => word.First( c => c >= '1' && c <= '9' ) )
+            .Aggregate( ( a, b ) => a + " " + b );
+    }
+
+}
---
+++ Tests/YourOrder.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+
+[TestFixture]
+public class SolutionTest
+{
+    [Test, Description( "Sample Tests" )]
+    public void SampleTest()
+    {
+        Assert.AreEqual( "Thi1s is2 3a T4est", YourOrder.Order( "is2 Thi1s T4est 3a" ) );
+        Assert.AreEqual( "Fo1r the2 g3ood 4of th5e pe6ople", YourOrder.Order( "4of Fo1r pe6ople g3ood th5e the2" ) );
+        Assert.AreEqual( "", YourOrder.Order( "" ) );
+    }
+}
