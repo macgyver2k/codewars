2721857a22a1db660086628597467bba84d3d792
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-08-07 22:05:41 +0200

    implemented SumParts challenge

--- Challenges/SumParts.cs
+++ Challenges/SumParts.cs
@@ -1,8 +1,14 @@
-using System;
 public class SumParts
 {
     public static int[] PartsSums(int[] ls)
     {
-        throw new NotImplementedException();
+        var result = new int[ls.Length + 1];
+
+        for (int i = ls.Length - 1; i >= 0; i--)
+        {
+            result[i] = ls[i] + result[i + 1];
+        }
+
+        return result;
     }
 }
