599ebee99109dbcd8214a907f45f8ff4380132c8
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-08-07 23:48:19 +0200

    implemented Persist

---
+++ Challenges/Persist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class Persist
+{
+    public static Int32 Persistence( Int64 n )
+    {
+        var product = n;
+        var digits = getDigits( product );
+
+        var i = 0;
+
+        for( ; digits.Length > 1; i++ )
+        {
+            product = digits.Aggregate( ( d, p ) => d * p );
+            digits = getDigits( product );
+        }
+
+        return i;
+    }
+
+    private static Int32[] getDigits( Int64 product )
+    {
+        return product.ToString().Select( c => new String( c, 1 ) ).Select( c => Int32.Parse( c ) ).ToArray();
+    }
+}
