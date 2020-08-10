2dae87057f1587b4bbfba70f3f4d47a5f164c35d
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-08-10 13:09:39 +0200

    implemented CrackThePin

---
+++ Challenges/CrackThePin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+public class CrackThePin
+{
+    public static String crack( String hash )
+    {
+        var md5 = MD5.Create();
+
+        for( var index = 0; index < 100000; index++ )
+        {
+            var pin = index.ToString( "00000" );
+            var bytes = System.Text.Encoding.UTF8.GetBytes( pin );
+            var pinHashBytes = md5.ComputeHash( bytes );
+            var pinHash = String.Join( "", pinHashBytes.Select( a => a.ToString( "x2" ) ) );
+
+            if( pinHash == hash )
+            {
+                return pin;
+            }
+        }
+
+        return "";
+    }
+}
---
+++ Tests/CrackThePinTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class CrackThePinTests
+{
+    [Test]
+    public void SimpleTest()
+    {
+        Assert.AreEqual( "12345", CrackThePin.crack( "827ccb0eea8a706c4c34a16891f84e7b" ) );
+    }
+    [Test]
+    public void HarderTest()
+    {
+        Assert.AreEqual( "00078", CrackThePin.crack( "86aa400b65433b608a9db30070ec60cd" ) );
+    }
+}
