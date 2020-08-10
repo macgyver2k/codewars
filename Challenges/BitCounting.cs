cf1a4a8b949a32039fc405fc11d826be1237922f
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-08-10 11:41:05 +0200

    implemented BitCounting

---
+++ Challenges/BitCounting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public class BitCounting
+{
+    public static Int32 CountBits( Int32 n )
+    {
+        var binaryString = Convert.ToString( n, 2 );
+        return binaryString.Aggregate( 0, ( a, b ) => a + ( b == '1' ? 1 : 0 ) );
+    }
+}
---
+++ Tests/BitCountingTest.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+
+[TestFixture]
+public class BitCountingTests
+{
+    [Test]
+    public void CountBits()
+    {
+        Assert.AreEqual( 0, BitCounting.CountBits( 0 ) );
+        Assert.AreEqual( 1, BitCounting.CountBits( 4 ) );
+        Assert.AreEqual( 3, BitCounting.CountBits( 7 ) );
+        Assert.AreEqual( 2, BitCounting.CountBits( 9 ) );
+        Assert.AreEqual( 2, BitCounting.CountBits( 10 ) );
+    }
+}
